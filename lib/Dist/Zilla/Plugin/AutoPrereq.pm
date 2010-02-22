450419330f76dce6a484b9a3f95af9fb41b2b182
Ricardo SIGNES <rjbs@cpan.org>
2010-02-22 09:06:41 -0500

    use Perl::PrereqScanner

--- lib/Dist/Zilla/Plugin/AutoPrereq.pm
+++ lib/Dist/Zilla/Plugin/AutoPrereq.pm
@@ -8,7 +8,9 @@ package Dist::Zilla::Plugin::AutoPrereq;
 use Dist::Zilla::Util;
 use Moose;
 use MooseX::Has::Sugar;
+use Perl::PrereqScanner;
 use PPI;
+use Version::Requirements;
 use version;
 
 with 'Dist::Zilla::Role::FixedPrereqs';
@@ -25,7 +27,8 @@ sub prereq {
     my $self = shift;
     my $files = $self->zilla->files;
 
-    my %prereqs;
+    my $req = Version::Requirements->new;
+
     my @modules;
     foreach my $file ( @$files ) {
         # parse only perl files
@@ -40,134 +43,28 @@ sub prereq {
         push @modules, $module;
 
         # parse a file, and merge with existing prereqs
-        my %fprereqs = $self->_prereqs_in_file($file);
-        foreach my $module ( keys %fprereqs ) {
-            my $version = $fprereqs{$module};
+        my $file_req = Perl::PrereqScanner->new->scan_string(
+            $file->content
+        );
 
-            if ( exists $prereqs{$module} ) {
-                # prereq already found, check if versions are specified
-                if ( _looks_like_version($version) ) {
-                    my $vold = version->new( $prereqs{$module} );
-                    my $vnew = version->new( $version );
-                    $prereqs{$module} = $version if $vnew > $vold;
-                }
-                # don't bother adding the new prereq, since no version
-                # is provided: therefore, the old module has either a
-                # more important version or is already undef, and
-                # there's no need to replace it
-
-            } else {
-                # new prereq, let's add it
-                $prereqs{$module} = $version;
-            }
-        }
+        $req->add_requirements($file_req);
     }
 
     # remove prereqs shipped with current dist
-    delete @prereqs{ @modules };
+    $req->clear_requirement($_) for @modules;
 
     # remove prereqs from skiplist
     if ( $self->has_skip && $self->skip ) {
         my $skip = $self->skip;
         my $re   = qr/$skip/;
-        my @deletes;
+
         foreach my $k ( keys %prereqs ) {
-            push @deletes, $k if $k =~ $re;
+            $req->clear_requirements($k) if $k =~ $re;
         }
-        delete @prereqs{ @deletes };
     }
 
     # we're done, return what we've found
-    return \%prereqs;
-}
-
-
-#-- private methods
-
-#
-# my %prereqs = $autoprereq->_prereqs_in_file( $file );
-#
-# find the prereqs (hash of module / version) in $file (a
-# dist::zilla:file::ondisk object) and return them.
-#
-sub _prereqs_in_file {
-    my ($self, $file) = @_;
-
-    my %prereqs;
-
-    my $p = Dist::Zilla::Util::Nonpod->_new;
-    $p->read_string( $file->content );
-    my $content = $p->_nonpod;
-    my $doc = PPI::Document->new( \$content );
-
-    # regular use and require
-    my $includes = $doc->find('Statement::Include') || [];
-    foreach my $node ( @$includes ) {
-        # minimum perl version
-        if ( $node->version ) {
-            $prereqs{perl} = $node->version;
-            next;
-        }
-
-        # skipping pragamata
-        next if $node->module ~~ [ qw{ strict warnings lib } ];
-
-        if ( $node->module ~~ [ qw{ base parent } ] ) {
-            # the content is in the 5th token
-            my $meat = $node->child(4);
-            my @parents = $meat->isa('PPI::Token::QuoteLike::Words')
-                ? ( $meat->literal )
-                : ( $meat->string  );
-            @prereqs{ @parents } = (0) x @parents;
-
-            # base is in perl core, parent isn't
-            next if $node->module eq 'base';
-        }
-
-        # regular modules
-        my $version = $node->module_version
-            ? $node->module_version->content
-            : 0;
-        $prereqs{ $node->module } = $version;
-    }
-
-    # for moose specifics, let's fetch top-level statements
-    my @statements =
-        grep { $_->child(0)->isa('PPI::Token::Word') }
-        grep { ref($_) eq 'PPI::Statement' } # no ->isa()
-        $doc->children;
-
-    # roles: with ...
-    my @roles =
-        map  { $_->child(2)->string }
-        grep { $_->child(0)->literal eq 'with' }
-        @statements;
-    @prereqs{ @roles } = (0) x @roles;
-
-    # inheritance: extends ...
-    my @bases =
-        map  { $_->string }
-        grep { $_->isa('PPI::Token::Quote') }
-        map  { $_->children }
-        grep { $_->child(0)->literal eq 'extends' }
-        @statements;
-    @prereqs{ @bases } = (0) x @bases;
-
-    return %prereqs;
-}
-
-
-# -- private subs
-
-#
-# my $bool = _looks_like_version( $string );
-#
-# return true if $string somehow looks like a perl version.
-#
-sub _looks_like_version {
-    my $version = shift;
-    return defined $version &&
-        $version =~ /\Av?\d+(?:\.[\d_]+)?\z/;
+    return $req->as_string_hash;
 }
 
 
